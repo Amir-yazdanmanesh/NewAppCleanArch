f1bda0b4c8dd5e98fae549d749923fddd4fcbb63
amir <amir.yazdanmanesh@gmail.com>
2024-10-30 00:02:17 +0330

    feat: add helper utilities for common tasks

---
+++ presentation/src/main/java/com/example/yazdanmanesh/ui/utils/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.yazdanmanesh.ui.utils
+
+object Constants {
+    const val SHIMMER_ITEM_COUNT = 6
+    const val SHIMMER_ANIMATION_DURATION = 1000
+    const val SHIMMER_ROUNDED_CORNER_SIZE = 10
+    const val DETAILS_ANIMATION_DELAY = 300L
+    const val FUNNY_BUTTON_ANIMATION_DURATION = 1500
+    const val FUNNY_TEXT_VISIBLE_DELAY = 500L
+    const val FUNNY_TEXT_STOP_DELAY = 1500L
+}
